b5540e8acad0d71e36b4aec77f3805f0d577bcbc
beyazsu <beyazsu001@gmail.com>
2018-11-03 22:17:37 +0300

    redux-promise-middleware modülü import edilerek reduxPromise fonksiyonu applymiddleware ye parametre olarak geçirildi

--- src/index.js
+++ src/index.js
@@ -9,16 +9,16 @@ import thunk from 'redux-thunk';
 import {composeWithDevTools} from 'redux-devtools-extension';
 import {Provider} from 'react-redux';
 import {BrowserRouter} from 'react-router-dom'; 
+import logger from 'redux-logger';
+import reduxPromise from 'redux-promise-middleware';
 
 
 const store = createStore(
     rootReducer,
     composeWithDevTools(
-        applyMiddleware(thunk)
+        applyMiddleware(reduxPromise(), thunk,logger)
     )
 );
-
-
 
 
 ReactDOM.render(
