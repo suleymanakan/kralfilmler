8c6d8de4fd6cd62da2dd5cb6cbf4ddc4f96363a1
beyazsu <beyazsu001@gmail.com>
2018-11-03 22:01:54 +0300

    initilstate oluşturuldu ve oluşturulan  switch/case yapısıyla acttionlardan gelen tanımlamalara gore durumlar oluşturuldu

--- src/sa_reducers/movies.js
+++ src/sa_reducers/movies.js
@@ -1,5 +1,24 @@
-export default (state=[], action)=>{
+import {FETCHED_MOVIES, FETCHED_MOVIES_ERROR} from '../sa_actions/Movies';
+
+const initialstate={
+    fetching:false,
+    fetched: false,
+    error: {},
+    movies: []
+
+}
+export default (state=initialstate, action)=>{
     switch(action.type){
+        case FETCHED_MOVIES:
+        return {
+            ...state,
+            movies: action.payload
+        }
+        case FETCHED_MOVIES_ERROR:
+        return{
+            ...state,
+            error: action.payload
+        }
         default:
            return state;
     }
