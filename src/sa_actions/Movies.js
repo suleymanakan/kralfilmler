647a82df28d8dd06290852747bd6a9c97bafd054
beyazsu <beyazsu001@gmail.com>
2018-11-16 18:25:04 +0300

    dispatch işlemi optimize edildi

--- src/sa_actions/Movies.js
+++ src/sa_actions/Movies.js
@@ -1,22 +1,27 @@
 import axios from 'axios';
 import { API_PATH } from '../sa_config/Ifadeler';
 
-export const FETCHED_MOVIES = "FETCHED_MOVIES";
-export const FETCHED_MOVIES_ERROR = "FETCHED_MOVIES_ERROR";
+export const FETCH_MOVIES_PENDING = "FETCH_MOVIES_PENDING";
+export const FETCH_MOVIES_FULFILLED = "FETCH_MOVIES_FULFILLED";
+export const FETCH_MOVIES_REJECTED = "FETCH_MOVIES_REJECTED";
 
 export function fetchMovies(){
     return dispatch => {
-        axios.get(API_PATH+'/movies')
-        .then(result=> result.data)
-        .then(data =>dispatch({
-            type: FETCHED_MOVIES,
-            payload: data.movies
-        }))
-        .catch(error=>dispatch({
-            type: FETCHED_MOVIES_ERROR,
-            payload: error
-        }))
+        /* axios.get(API_PATH+"/movies")
+         .then(result=>result.data)
+         .then(data=>dispatch({
+             type:FETCH_MOVIES_FULFILLED,
+             payload: data.movies
+         }))
+         .catch(error=>dispatch({
+             type:FETCH_MOVIES_REJECTED,
+             payload:error
+         }))*/
         
-
+       dispatch({
+          type: "FETCH_MOVIES",
+          payload: axios.get(`${API_PATH}/movies`)
+          .then(result=>result.data.movies)
+      })
     }
 };
