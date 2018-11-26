aa88e7239ce8b4c4d2d6c5ca3f2f390daf752fae
beyazsu <beyazsu001@gmail.com>
2018-11-26 23:23:51 +0300

    Film silme işlemi için gerekli action fonksionu yazıldı

--- src/sa_actions/Movies.js
+++ src/sa_actions/Movies.js
@@ -5,9 +5,35 @@ export const FETCH_MOVIES_PENDING = "FETCH_MOVIES_PENDING";
 export const FETCH_MOVIES_FULFILLED = "FETCH_MOVIES_FULFILLED";
 export const FETCH_MOVIES_REJECTED = "FETCH_MOVIES_REJECTED";
 
+export const DELETE_MOVIE_PENDING = "DELETE_MOVIE_PENDING";
+export const DELETE_MOVIE_FULFILLED = "DELETE_MOVIE_FULFILLED";
+export const DELETE_MOVIE_REJECTED = "DELETE_MOVIE_REJECTED";
+
 export function fetchMovies(){
     return dispatch => {
-        /* axios.get(API_PATH+"/movies")
+        
+       dispatch({
+          type: "FETCH_MOVIES",
+          payload: axios.get(`${API_PATH}/movies`)
+          .then(result=>result.data.movies)
+      })
+    }
+};
+
+export function onDeleteMovie(id){
+    return dispatch => {
+        dispatch({
+            type : "DELETE_MOVİE",
+            payload: axios.delete(`${API_PATH}/movies/${id}`)
+            .then(result=> Object.assign({}, result, {id}))
+        })
+    }
+}
+
+
+
+
+/* axios.get(API_PATH+"/movies")
          .then(result=>result.data)
          .then(data=>dispatch({
              type:FETCH_MOVIES_FULFILLED,
@@ -17,11 +43,3 @@ export function fetchMovies(){
              type:FETCH_MOVIES_REJECTED,
              payload:error
          }))*/
-        
-       dispatch({
-          type: "FETCH_MOVIES",
-          payload: axios.get(`${API_PATH}/movies`)
-          .then(result=>result.data.movies)
-      })
-    }
-};
